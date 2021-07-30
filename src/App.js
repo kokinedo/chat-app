8d0142cf4b10a8d17a96d236fad58660409c83ce
kokinedo <kokinedo@gmail.com>
2021-07-30 05:06:32 -0400

    Added handling for incorrect creditials

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
+  if(!localStorage.getItem('username')) return<LoginForm />
+
   return(
     <ChatEngine 
         height="100vh"
         projectID="088d7bd9-b7bb-455b-bd72-d981c3c8c61a"
-        userName="kokinedo"
-        userSecret="123123"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
     />
   );
